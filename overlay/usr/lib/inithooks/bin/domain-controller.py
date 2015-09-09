168b6c79a628847194533c7e43da299f7a050c79
Jeremy Davis <jeremy@turnkeylinux.org>
2015-09-09 08:40:33 +0000

    major updates for v14.0

--- overlay/usr/lib/inithooks/bin/domain-controller.py
+++ overlay/usr/lib/inithooks/bin/domain-controller.py
@@ -4,17 +4,20 @@
 
 Options:
     --pass=      if not provided, will ask interactively
+    --realm=     if not provided, will ask interactively
+                 DEFAULT=domain.lan
     --domain=    if not provided, will ask interactively
                  DEFAULT=DOMAIN
 """
 
-import re
 import sys
 import getopt
 import subprocess
 from subprocess import PIPE
+from os import remove
+from string import digits, ascii_uppercase, ascii_lowercase, punctuation
 
-from executil import system
+from executil import system, getoutput
 from dialog_wrapper import Dialog
 
 def usage(s=None):
@@ -28,24 +31,47 @@ def fatal(s):
     print >> sys.stderr, "Error:", s
     sys.exit(1)
 
+ADMIN_USER="administrator"
+
 DEFAULT_DOMAIN="DOMAIN"
+DEFAULT_REALM="domain.lan"
+
+HOSTNAME=getoutput('hostname -s').strip()
+NET_IP=getoutput('hostname -I').strip()
+
+NET_IP321=NET_IP.split('.')[:-1]
+NET_IP321.reverse()
+NET_IP321='.'.join(NET_IP321)
+NET_IP4=NET_IP.split('.')[-1]
 
 def main():
     try:
         opts, args = getopt.gnu_getopt(sys.argv[1:], "h",
-                                       ['help', 'pass=', 'domain='])
+                                       ['help', 'pass=', 'realm=', 'domain='])
     except getopt.GetoptError, e:
         usage(e)
 
+    realm = ""
     domain = ""
-    password = ""
+    admin_password = ""
     for opt, val in opts:
         if opt in ('-h', '--help'):
             usage()
         elif opt == '--pass':
-            password = val
+            admin_password = val
+        elif opt == '--realm':
+            realm = val
+            DEFAULT_DOMAIN = realm.split('.')[0].upper()
         elif opt == '--domain':
             domain = val
+
+    if not realm:
+        d = Dialog('Turnkey Linux - First boot configuration')
+        realm = d.get_input(
+            "Samba/Kerberos Realm",
+            "Enter realm you would like to use.",
+            DEFAULT_REALM)
+        DEFAULT_DOMAIN = realm.split('.')[0].upper()
 
     if not domain:
         d = Dialog('TurnKey Linux - First boot configuration')
@@ -54,48 +80,44 @@ def main():
             "Enter domain you would like to use.",
             DEFAULT_DOMAIN)
 
-    if not password:
+    if not admin_password:
         if 'd' not in locals():
             d = Dialog('TurnKey Linux - First boot configuration')
 
-        password = d.get_password(
-            "Samba Password",
-            "Enter new password for the samba 'administrator' account.")
+        admin_password = d.get_password(
+                "Samba Password",
+                "Enter new password for the samba 'administrator' account.", pass_req=r'(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}')
 
+    # stop Samba service(s) - in case it's already running
     system("/etc/init.d/samba stop >/dev/null || true")
+    system("/etc/init.d/samba-ad-dc stop >/dev/null || true")
 
-    # set domain
-    if domain == "DEFAULT":
-        domain = DEFAULT_DOMAIN
+    # just in case Samba4 has been set up Samba3 style
+    system("/etc/init.d/smbd stop >/dev/null || true")
+    system("/etc/init.d/nmbd stop >/dev/null || true")
 
-    new = []
-    smbconf = "/etc/samba/smb.conf"
-    for s in file(smbconf).readlines():
-        s = s.rstrip()
-        s = re.sub("workgroup = (.*)", "workgroup = %s" % domain.upper(), s)
-        new.append(s)
+    remove('/etc/samba/smb.conf')
 
-    fh = file(smbconf, "w")
-    print >> fh, "\n".join(new)
-    fh.close()
+    system('samba-tool domain provision --realm {REALM} --domain {DOMAIN} --adminpass {ADMIN_PASSWORD} --server-role=dc --use-rfc2307 --option="dns forwarder = 8.8.8.8"'.format(REALM = realm, DOMAIN = domain, ADMIN_PASSWORD = admin_password))
 
-    # set unix administrator password
-    p = subprocess.Popen(["chpasswd"], stdin=PIPE, shell=False)
-    p.stdin.write("administrator:%s" % password)
-    p.stdin.close()
-    err = p.wait()
-    if err:
-        fatal(err)
+    system('samba-tool user setexpiry {ADMIN_USER} --noexpiry'.format(ADMIN_USER=ADMIN_USER))
 
-    # set smb administrator password
-    p = subprocess.Popen(["smbpasswd","-s","administrator"], stdin=PIPE, shell=False)
-    p.stdin.write("%s\n%s" % (password, password))
-    p.stdin.close()
-    err = p.wait()
-    if err:
-        fatal(err)
+    system('samba-tool domain exportkeytab /etc/krb5.keytab')
 
-    system("/etc/init.d/samba start >/dev/null || true")
+    system('chown root:root /etc/krb5.keytab; chmod 600 /etc/krb5.keytab')
+    
+    system('ln -sf /var/lib/samba/private/krb5.conf /etc/krb5.conf')
+ 
+    system('sed -i "s/domain.*/domain {REALM}/" /etc/resolvconf/resolv.conf.d/head'.format(REALM = realm))
+    system('sed -i "s/search.*/search {REALM}/" /etc/resolvconf/resolv.conf.d/head'.format(REALM = realm))
+
+    system('service samba-ad-dc start')
+
+    system('sleep 5')
+
+    system('echo {ADMIN_PASSWORD} | kinit {ADMIN_USER}@{REALM}'.format(ADMIN_PASSWORD=admin_password, ADMIN_USER=ADMIN_USER, REALM=realm.upper()))
+
+    system("/etc/init.d/samba-ad-dc restart >/dev/null || true")
 
 if __name__ == "__main__":
     main()
