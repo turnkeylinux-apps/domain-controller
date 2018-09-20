cc9b10aab12eed9ce9a5922c893bac1a09ea468e
Jeremy Davis <jeremy@turnkeylinux.org>
2018-09-20 15:03:00 +1000

    some tweaks after reading samba docs

--- overlay/usr/lib/inithooks/bin/sambaconf_add.sh
+++ overlay/usr/lib/inithooks/bin/sambaconf_add.sh
@@ -12,14 +12,23 @@ while getopts d:r:u:p: option
 done
 
 # stop Samba service(s) - in case it's already running
-/etc/init.d/samba stop >/dev/null || true
-/etc/init.d/samba-ad-dc stop >/dev/null || true
+service samba stop >/dev/null || true
+service samba-ad-dc stop >/dev/null || true
 
 # just in case Samba4 has been set up Samba3 style
-/etc/init.d/smbd stop >/dev/null || true
-/etc/init.d/nmbd stop >/dev/null || true
+service smbd stop >/dev/null || true
+service nmbd stop >/dev/null || true
 
-rm /etc/samba/smb.conf || true
+# remove conf files
+CONF=$(smbd -b | grep "CONFIGFILE" | cut -d: -f2)
+rm -f $CONF
+rm -f /etc/krb5.conf
+
+# clean up *.tdb and *.ldb files (samba DBs)
+DIRS=$(smbd -b | egrep "LOCKDIR|STATEDIR|CACHEDIR|PRIVATE_DIR")
+for dir in $DIRS; do
+    find $dir \( -name "*.tdb" -or -name "*.ldb" \) -delete
+done
 
 samba-tool domain provision --realm $REALM --domain $DOMAIN --adminpass $ADMIN_PASSWORD --server-role=dc --use-rfc2307 --option="dns forwarder = 8.8.8.8"
 
@@ -27,19 +36,18 @@ samba-tool user setexpiry $ADMIN_USER --noexpiry
 
 samba-tool domain exportkeytab /etc/krb5.keytab
 
-chown root:root /etc/krb5.keytab; chmod 600 /etc/krb5.keytab
+chown root:root /etc/krb5.keytab
+chmod 600 /etc/krb5.keytab
 
-ln -sf /var/lib/samba/private/krb5.conf /etc/krb5.conf
+cp /var/lib/samba/private/krb5.conf /etc/krb5.conf
  
-sed -i "s/domain.*/domain $REALM/" /etc/resolvconf/resolv.conf.d/head
-sed -i "s/search.*/search $REALM/" /etc/resolvconf/resolv.conf.d/head
+sed -i "s/^search .*/search $REALM/" /etc/resolvconf/resolv.conf.d/head
+sed -i "s/^domain .*/domain $REALM/" /etc/resolvconf/resolv.conf.d/head
 
 service samba-ad-dc start
 
 sleep 5
 
-REALM=$(echo "$REALM" | tr '[:lower:]' '[:upper:]')
+echo $ADMIN_PASSWORD | kinit $ADMIN_USER
 
-echo $ADMIN_PASSWORD | kinit $ADMIN_USER@$REALM
-
-/etc/init.d/samba-ad-dc restart >/dev/null || true
+service samba-ad-dc start
--- overlay/usr/lib/inithooks/bin/sambaconf_join.sh
+++ overlay/usr/lib/inithooks/bin/sambaconf_join.sh
@@ -15,21 +15,30 @@ done
 # update nameservers
 if [ ${#NAME_SERVER} -gt 0 ]
 then
-    sed -i "s/nameserver.*/nameserver $NAME_SERVER/" /etc/resolvconf/resolv.conf.d/head
+    sed -i "s/^nameserver .*/nameserver $NAME_SERVER/" /etc/resolvconf/resolv.conf.d/head
     resolvconf -u
 fi
 
 # stop Samba service(s) - in case it's already running
-/etc/init.d/samba stop >/dev/null || true
-/etc/init.d/samba-ad-dc stop >/dev/null || true
+service samba stop >/dev/null || true
+service samba-ad-dc stop >/dev/null || true
 
 # just in case Samba4 has been set up Samba3 style
-/etc/init.d/smbd stop >/dev/null || true
-/etc/init.d/nmbd stop >/dev/null || true
+service smbd stop >/dev/null || true
+service nmbd stop >/dev/null || true
 
 hostname dc2
 
-rm /etc/samba/smb.conf || true
+# remove conf files
+CONF=$(smbd -b | grep "CONFIGFILE" | cut -d: -f2)
+rm -f $CONF
+rm -f /etc/krb5.conf
+
+# clean up *.tdb and *.ldb files (samba DBs)
+DIRS=$(smbd -b | egrep "LOCKDIR|STATEDIR|CACHEDIR|PRIVATE_DIR")
+for dir in $DIRS; do
+    find $dir \( -name "*.tdb" -or -name "*.ldb" \) -delete
+done
 
 samba-tool domain join $REALM DC -U"$DOMAIN\\$ADMIN_USER" --password=$ADMIN_PASSWORD --dns-backend=SAMBA_INTERNAL
 
@@ -37,20 +46,17 @@ samba-tool user setexpiry $ADMIN_USER --noexpiry
 
 samba-tool domain exportkeytab /etc/krb5.keytab
 
-chown root:root /etc/krb5.keytab; chmod 600 /etc/krb5.keytab
+chown root:root /etc/krb5.keytab
+chmod 600 /etc/krb5.keytab
+cp /var/lib/samba/private/krb5.conf /etc/krb5.conf
 
-ln -sf /var/lib/samba/private/krb5.conf /etc/krb5.conf
- 
-sed -i "s/domain.*/domain $REALM/" /etc/resolvconf/resolv.conf.d/head
-sed -i "s/search.*/search $REALM/" /etc/resolvconf/resolv.conf.d/head
+sed -i "s/^search .*/search $REALM/" /etc/resolvconf/resolv.conf.d/head
+sed -i "s/^domain .*/domain $REALM/" /etc/resolvconf/resolv.conf.d/head
 
 service samba-ad-dc start
 
 sleep 5
 
-REALM=$(echo "$REALM" | tr '[:lower:]' '[:upper:]')
+echo $ADMIN_PASSWORD | kinit $ADMIN_USER
 
-echo $ADMIN_PASSWORD | kinit $ADMIN_USER@$REALM
-
-/etc/init.d/samba-ad-dc restart >/dev/null || true
-
+service samba-ad-dc start
